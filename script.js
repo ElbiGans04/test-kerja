bed10a58599bce4d70b6c0579a9161f65c2446c8
rhafael bijaksana <rhafaelbijaksana04@gmail.com>
2021-09-28 13:33:07 +0700

    logic untuk maze 2

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
-// MAZE 1
+// // MAZE 1
 
 const count = 15;
-const listSuku = dapatkanKumpulanSuku(count, 1, 4);
-const listSuku2 = dapatkanKumpulanSuku(count,3, 4);
+// const listSuku = dapatkanKumpulanSuku(count, 1, 4);
+// const listSuku2 = dapatkanKumpulanSuku(count, 3, 4);
+// for (let i = 1; i <= count; i++) {
+//   let column = ``;
+//   let special = listSuku.includes(i);
+//   let special2 = listSuku2.includes(i);
+//   for (let j = 1; j <= count; j++) {
+//     if (i % 2 === 0) {
+//       if (j === 1 || j === count) column += `@`;
+//       else column += ` `;
+//     } else {
+//       if ((special && j === 2) || (special2 && j === count - 1)) column += " ";
+//       else column += `@`;
+//     }
+//   }
 
-for (let i = 1; i <= count; i++) {
-    let column = ``;
-    let special = listSuku.includes(i);
-    let special2 = listSuku2.includes(i);
-    for (let j=1; j <= count; j++) {
-        if ((i % 2) === 0) {
-            if (j === 1 || j === count) column += `@`;
-            else column += ` `;
-        } else {
-            if ((special && j === 2) || (special2 && j === (count - 1))) column += ' ';
-            else column += `@`;
-        }
-    }
-
-    console.log(column, i)
-}
+//   // console.log(column, i)
+// }
 
 function dapatkanKumpulanSuku(limit, a, b) {
   let list = [];
@@ -27,9 +26,64 @@ function dapatkanKumpulanSuku(limit, a, b) {
   let stop = false;
   while (stop === false) {
     let result = a + (i++ - 1) * b;
-    if (result <= limit ) list.push(result);
-    else stop = true;
+    if (b < 0 ) {
+      if (result >= limit) list.push(result);
+      else stop = true;
+    } else {
+      if (result <= limit) list.push(result);
+      else stop = true;
+    }
+
+     if (i === 100) stop = true;
   }
 
   return list;
 }
+
+function removeDuplicateArray (array) {
+  return array.filter((arrayItem, index) => array.indexOf(arrayItem) == index)
+}
+
+
+// Panjang list suku harus genap
+const sukuList = dapatkanKumpulanSuku(count, 1, 2);
+const sukuTengah = sukuList.length % 2 === 0 ? sukuList[sukuList.length / 2] : null;
+const sukuTengah2 = sukuList.length % 2 === 0 ? sukuList[(sukuList.length / 2) - 1] : null;
+// Nilai
+const sukuValue = [...dapatkanKumpulanSuku(sukuTengah, 3, 2 ), ...dapatkanKumpulanSuku(1, sukuTengah2, - 2 )]
+const sukuValue2 = [...dapatkanKumpulanSuku(sukuTengah, count, -2 ), ...dapatkanKumpulanSuku(count, sukuTengah, 2 )]
+
+// Simulasi 
+// Misal index 1,3,5
+// Mencari bagian sukuValue.slice(0, sukuList.indexOf(5) + 1)
+// Jika 
+// Suku Ganjil
+// [
+  //   1,  3,  5,  7,
+  //   9, 11, 13, 15
+  // ] 
+  //  Nilai Suku 
+  // [
+    //   3, 5, 7, 9,
+    //   7, 5, 3, 1
+    // ]
+// pecah lagi
+const index = 1;
+const fil = removeDuplicateArray(sukuValue.filter((value) => sukuValue[index] >= value).sort((a, b) => a - b));
+const fil2 = removeDuplicateArray(sukuValue2.filter((value) => sukuValue2[index] <= value).sort((a,b) => a-b));
+// Nilai batas kiri terbesar
+const filLeft = Math.max(...fil);
+// Nilai batas kanan terkecil
+const filRight = Math.min(...fil2)
+
+// Check isi
+let column = ``;
+for (let i = 1; i <= count; i++) {
+  if (fil.includes(i) || fil2.includes(i)) column += `@`
+  else {
+    if(i > filLeft && i < filRight) column += `@`
+    else column += ' '
+  }
+}
+console.log(column)
+
