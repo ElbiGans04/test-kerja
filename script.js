f6cf36a57d3c88642343087e0f79db89a414570d
rhafael bijaksana <rhafaelbijaksana04@gmail.com>
2021-09-29 16:02:42 +0700

    menyelesaikan maze ke tiga

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 // // MAZE 1
-const count = 15;
-const listSuku = dapatkanKumpulanSuku(count, 1, 4);
-const listSuku2 = dapatkanKumpulanSuku(count, 3, 4);
-for (let i = 1; i <= count; i++) {
-  let column = ``;
-  let special = listSuku.includes(i);
-  let special2 = listSuku2.includes(i);
+const count = 27;
+// const listSuku = dapatkanKumpulanSuku(count, 1, 4);
+// const listSuku2 = dapatkanKumpulanSuku(count, 3, 4);
+// for (let i = 1; i <= count; i++) {
+//   let column = ``;
+//   let special = listSuku.includes(i);
+//   let special2 = listSuku2.includes(i);
 
-  for (let j = 1; j <= count; j++) {
-    // Jika baris genap
-    if (i % 2 === 0) {
-      if (j === 1 || j === count) column += `@`;
-      else column += ` `;
-    } else {
-      if ((special && j === 2) || (special2 && j === count - 1)) column += " ";
-      else column += `@`;
-    }
-  }
+//   for (let j = 1; j <= count; j++) {
+//     // Jika baris genap
+//     if (i % 2 === 0) {
+//       if (j === 1 || j === count) column += `@`;
+//       else column += ` `;
+//     } else {
+//       if ((special && j === 2) || (special2 && j === count - 1)) column += " ";
+//       else column += `@`;
+//     }
+//   }
 
-  console.log(column)
-}
+//   console.log(column)
+// }
 
 
 function dapatkanKumpulanSuku(limit, a, b) {
@@ -102,3 +102,78 @@ function removeDuplicateArray(array) {
 
 //   console.log(column)
 // }
+
+
+// MAZE 3
+const sukuList = dapatkanKumpulanSuku(count, 1, 2); // dapatkan kumpulan bilangan ganjil
+const sukuListGenap = (sukuList.length % 2) === 0 ? true : false;
+const sukuTengah = sukuListGenap ? sukuList[sukuList.length / 2] : null;
+const sukuTengah2 = sukuListGenap ? sukuList[sukuList.length / 2 - 1] : null;
+// List Index Khusus
+const sukuValue = [
+  ...dapatkanKumpulanSuku(sukuTengah, 3, 2),
+  ...dapatkanKumpulanSuku(1, sukuTengah2, -2),
+];
+const sukuValue2 = [
+  ...dapatkanKumpulanSuku(sukuTengah, count, -2),
+  ...new Array(Math.ceil(sukuList.length / 4)).fill(sukuTengah),
+  ...dapatkanKumpulanSuku(count - 2, sukuTengah + 2, 2),
+];
+
+// Dapatkan batas baris terakhir
+const fil2 = removeDuplicateArray(
+  sukuValue2
+    .filter((value) => sukuValue2[sukuList.indexOf(count)] <= value)
+    .sort((a, b) => a - b)
+);
+
+// TINGGAL DAPETIN POSISI Y
+const X = dapatkanKumpulanSuku(sukuTengah, sukuList.length, 1);
+const Y = dapatkanKumpulanSuku(sukuTengah + fil2.length, sukuTengah, 3)
+console.log(X, Y)
+// looping
+for (let index = 1; index <= count; index++) {
+  const genap = index % 2 === 0 ? true : false;
+  // Nilai untuk baris genap
+  let tambah = sukuTengah < index ? (index + 1) : (index - 1);
+  // Ambil posisi index, genap dan ganjil berbeda penalaran
+  let index2 = genap ? sukuList.indexOf(tambah) : sukuList.indexOf(index);
+  // Ambil List posisi khusus
+  const fil = removeDuplicateArray(
+    sukuValue
+      .filter((value) => sukuValue[index2] >= value)
+      .sort((a, b) => a - b)
+  );
+  const fil2 = removeDuplicateArray(
+    sukuValue2
+      .filter((value) => sukuValue2[index2] <= value)
+      .sort((a, b) => a - b)
+  );
+  // Nilai batas kiri terbesar
+  const filLeft = Math.max(...fil);
+  // Nilai batas kanan terkecil
+  const filRight = Math.min(...fil2);
+
+  
+  let column = ``;
+  for (let i = 1; i <= count; i++) {
+    // Jika harus keluar
+    if (Y.includes(index) && X.includes(i)) {
+      Y[Y.indexOf(index)] = null;
+      X[X.indexOf(i)] = null;
+      column += ` ` 
+    } else {
+      // Jika i termasuk batasan khusus
+      if (fil.includes(i) || fil2.includes(i)) column += `@`;
+      else {
+        const value = genap ? " " : "@";
+        // Jika i berada diantara filLeft dan filRight
+        if (i > filLeft && i < filRight) column += `${value}`;
+        else column += " ";
+      }
+    }
+  }
+    
+  console.log(column)
+  
+}
